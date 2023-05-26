c26a09630e7b86ce968d481bf8a94a291777b3fa
knoxpo <enochrathod98@gmail.com>
2023-05-26 17:44:47 +0530

    BLE Scanning and Connection Implementation

--- android/app/src/main/kotlin/com/example/bluetooth_application/BluetoothService.kt
+++ android/app/src/main/kotlin/com/example/bluetooth_application/BluetoothService.kt
@@ -1,14 +1,22 @@
 package com.example.bluetooth_application
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Service
 import android.bluetooth.*
+import android.bluetooth.le.BluetoothLeScanner
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanFilter
+import android.bluetooth.le.ScanResult
+import android.bluetooth.le.ScanSettings
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.os.*
 import android.util.Log
+import androidx.core.app.ActivityCompat
 
 class BluetoothService : Service() {
 
@@ -20,7 +28,76 @@ class BluetoothService : Service() {
     private var mBluetoothManager: BluetoothManager? = null
     private lateinit var bondedDevicesArray: Array<BluetoothDevice>
     private var sendReceive: BluetoothDataService.SendReceive? = null
+    private lateinit var mBluetoothScanner: BluetoothLeScanner
+    private val scannedDevicesArrayBLE = mutableListOf<ScanResult>()
 
+    private val bleScanner by lazy {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            mBluetoothAdapter?.bluetoothLeScanner
+        } else {
+            TODO("VERSION.SDK_INT < LOLLIPOP")
+        }
+    }
+
+    private val scanCallback = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        object : ScanCallback() {
+            @SuppressLint("MissingPermission")
+            override fun onScanResult(callbackType: Int, result: ScanResult) {
+                val indexQuery =
+                    scannedDevicesArrayBLE.indexOfFirst { it.device.address == result.device.address }
+                if (indexQuery != -1) { // A scan result already exists with the same address
+                    scannedDevicesArrayBLE[indexQuery] = result
+                } else {
+                    with(result.device) {
+                        Log.e(
+                            TAG,
+                            "BLE ----> Scanned BLE device! Name: ${name ?: "Unnamed"}, address: $address"
+                        )
+                    }
+                    scannedDevicesArrayBLE.add(result)
+                }
+
+            }
+        }
+    } else {
+        TODO("VERSION.SDK_INT < LOLLIPOP")
+    }
+
+
+
+    private val gattCallback = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+        object : BluetoothGattCallback() {
+            @SuppressLint("MissingPermission")
+            override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
+                val deviceAddress = gatt.device.address
+
+                if (status == BluetoothGatt.GATT_SUCCESS) {
+                    if (newState == BluetoothProfile.STATE_CONNECTED) {
+                        Log.w(TAG, "Successfully connected to $deviceAddress")
+                        // TODO: Store a reference to BluetoothGatt
+                    } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                        Log.w(
+                            TAG,
+                            "Successfully disconnected from $deviceAddress"
+                        )
+                        gatt.close()
+                    }
+                } else {
+                    Log.w(
+                        TAG,
+                        "Error $status encountered for $deviceAddress! Disconnecting..."
+                    )
+                    gatt.close()
+                }
+            }
+
+            override fun onServicesDiscovered(gatt: BluetoothGatt?, status: Int) {
+                super.onServicesDiscovered(gatt, status)
+            }
+        }
+    } else {
+        TODO("VERSION.SDK_INT < JELLY_BEAN_MR2")
+    }
 
     override fun onCreate() {
         super.onCreate()
@@ -28,7 +105,6 @@ class BluetoothService : Service() {
             mBluetoothManager =
                 applicationContext.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
             mBluetoothAdapter = mBluetoothManager!!.adapter
-
         }
 
         val filter = IntentFilter()
@@ -57,13 +133,17 @@ class BluetoothService : Service() {
                         Log.e(TAG, "Bluetooth off")
                         enableBluetooth()
                     }
+
                     BluetoothAdapter.STATE_TURNING_OFF -> {
                         Log.e(TAG, "Bluetooth turning off")
                     }
+
                     BluetoothAdapter.STATE_ON -> {
                         Log.e(TAG, "Bluetooth On")
-                        getPairedDevices()
+                        //   getPairedDevices()
+                        initBLE()
                     }
+
                     BluetoothAdapter.STATE_TURNING_ON -> {
                         Log.e(TAG, "Bluetooth turning on")
                     }
@@ -79,9 +159,37 @@ class BluetoothService : Service() {
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
             applicationContext.startActivity(intent)
         } else {
-            getPairedDevices()
+            //getPairedDevices()
+            initBLE()
         }
     }
+
+    @SuppressLint("MissingPermission")
+    private fun initBLE() {
+        val scanSettings = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            ScanSettings.Builder()
+                .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+                .build()
+        } else {
+            TODO("VERSION.SDK_INT < LOLLIPOP")
+        }
+        Log.e(TAG, "DISCOVER--> START SCANNING")
+        bleScanner?.startScan(null, scanSettings, scanCallback)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            bleScanner?.stopScan(scanCallback)
+            for (i in scannedDevicesArrayBLE.indices){
+                if (scannedDevicesArrayBLE[i].device.address == "F5:51:98:6A:26:E9"){
+                    with(scannedDevicesArrayBLE[i].device){
+                        connectGatt(this@BluetoothService, false, gattCallback)
+                    }
+                }
+            }
+
+            Log.e(TAG, "DISCOVER--> STOP SCANNING")
+        }, 20000)
+    }
+
 
     @SuppressLint("MissingPermission")
     fun getPairedDevices() {
@@ -109,10 +217,12 @@ class BluetoothService : Service() {
                         Log.e(TAG, "DISCOVER--> ACTION_STATE_CHANGED")
 
                     }
+
                     BluetoothAdapter.ACTION_DISCOVERY_STARTED -> {
                         Log.e(TAG, "DISCOVER--> ACTION_DISCOVERY_STARTED")
 
                     }
+
                     BluetoothAdapter.ACTION_DISCOVERY_FINISHED -> {
                         Log.e(TAG, "DISCOVER--> ACTION_DISCOVERY_FINISHED")
 
@@ -135,6 +245,7 @@ class BluetoothService : Service() {
                                         "DISCOVER--> Scanned Device: ${device.name} + ${device.address}  --> NONE"
                                     )
                                 }
+
                                 BluetoothDevice.BOND_BONDING -> {
                                     Log.e(
                                         TAG,
@@ -142,6 +253,7 @@ class BluetoothService : Service() {
                                     )
 
                                 }
+
                                 BluetoothDevice.BOND_BONDED -> {
                                     Log.e(
                                         TAG,
@@ -199,6 +311,7 @@ class BluetoothService : Service() {
                     TAG,
                     "DISCOVER--> STATE_CONNECTION_FAILED"
                 )
+
                 Constants.STATE_MESSAGE_RECEIVED -> {
                     val readBuff = message.obj as ByteArray
                     val tempMsg = String(readBuff, 0, message.arg1)
